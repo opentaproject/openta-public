acd54ccc4e174d5f9dbf8cab1d05c0d993139d00
Hampus Linander <hampusl@gmail.com>
2016-08-30 09:55:11 +0200

    Added questiontypes directory.

---
+++ questiontypes/compare_numeric/__init__.py
@@ -0,0 +1,30 @@
+from exercises.question import register_question_type
+import functools
+import operator
+import exercises.symbolic as symbolic
+from exercises.util import compose
+
+# import exercises.question
+
+
+def parse_variables(variables):  # {{{
+    rawvars = variables.split(';')
+    pipeline = compose(
+        functools.partial(filter, operator.truth),
+        functools.partial(map, lambda x: x.split('=')),
+        functools.partial(map, lambda x: {'name': x[0].strip(), 'value': x[1].strip()}),
+    )
+    variables = list(pipeline(rawvars))
+    return variables  # }}}
+
+
+def question_check_compare_numeric(question_json, answer_data):
+    variables = parse_variables(question_json['variables']['$'])
+    correct = question_json['expression']['$']
+    result = symbolic.compare_numeric(variables, answer_data, correct)
+    latex = {'latex': symbolic.to_latex(answer_data)}
+    result.update(latex)
+    return result
+
+
+register_question_type('compareNumeric', question_check_compare_numeric)
