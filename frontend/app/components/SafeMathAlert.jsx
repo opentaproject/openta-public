374ac0019464f516bab7c1da842575a2c9ba4cf9
Hampus Linander <hampusl@gmail.com>
2016-10-13 11:57:54 +0200

    Added missing component file for SafeMathAlert to frontend.

---
+++ frontend/app/components/SafeMathAlert.jsx
@@ -0,0 +1,33 @@
+import React, { PropTypes, Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class SafeMathAlert extends Component {
+  constructor() {
+    super();
+  }
+
+  static propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string
+  }
+
+  render() {
+    var typeToClass = {
+      success: 'uk-alert-success',
+      warning: 'uk-alert-warning',
+      error: 'uk-alert-danger'
+    }
+    var typeClass = this.props.type ? typeToClass[this.props.type] : "";
+    try {
+      var rendered = {
+        __html: katex.renderToString(this.props.message)
+      };
+      return (
+         <div className={"uk-alert " + typeClass} ref="alert" dangerouslySetInnerHTML={rendered}></div>
+      );
+    }
+    catch(e) {
+      return (<div className={"uk-alert " + typeClass} ref="alert"/>);
+    }
+  }
+}
