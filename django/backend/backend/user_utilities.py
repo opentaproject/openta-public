772e3c2f5a5958cc34b774bfdcc71cf93b03bae1
Hampus Linander <hampusl@gmail.com>
2016-10-24 09:29:52 +0200

    Updated mail routines to mailgun.

--- django/backend/backend/user_utilities.py
+++ django/backend/backend/user_utilities.py
@@ -1,10 +1,16 @@
 from django.core.signing import TimestampSigner, BadSignature, SignatureExpired
 from django.shortcuts import reverse
-from django.core.mail import send_mail
+
+# from django.core.mail import send_mail
 from django.core.exceptions import ObjectDoesNotExist
 from course.models import Course
 from django.template.loader import get_template
 from django.template import Context
+from django.utils.translation import ugettext as _
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def create_activation_link(username, reverse_name='user-activation'):
@@ -35,7 +41,22 @@ def send_activation_mail(username, email, reverse_name='user-activation'):
         pass
     context = Context(pcontext)
     rendered_email = template.render(context)
-    send_mail(
-        'Account activation', rendered_email, sender + '@openta.se', [email], fail_silently=False
-    )
+    # send_mail('Account activation', rendered_email, sender + '@openta.se', [email], fail_silently=False)
+    mailgun_key = None
+    with open('mailgun_key', 'r') as f:
+        mailgun_key = f.readline().strip()
+    if not mailgun_key:
+        logger.error("No mailgun key found")
+    else:
+        r = requests.post(
+            "https://api.mailgun.net/v3/openta.se/messages",
+            auth=("api", mailgun_key),
+            data={
+                "from": sender + " <" + sender + "@openta.se>",
+                "to": [email],
+                "subject": sender + _(" account activation"),
+                "text": rendered_email,
+            },
+        )
+        logger.info("Sent activation mail to email " + email + ", response: " + r.body)
     return activate_url
