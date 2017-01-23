313e4edb98a9a838bef819602647e76e3eefc0a0
Hampus Linander <hampusl@gmail.com>
2017-01-24 00:11:46 +0100

    Removed loading of removed clonednumeric module and component.

--- django/backend/exercises/questiontypes/__init__.py
+++ django/backend/exercises/questiontypes/__init__.py
@@ -1,3 +1,2 @@
 from . import compare_numeric
 from . import multiple_choice
-from . import cloned_compare_numeric
--- frontend/app/components/questiontypes/question_types.js
+++ frontend/app/components/questiontypes/question_types.js
@@ -1,3 +1,2 @@
 import QuestionCompareNumeric from './QuestionCompareNumeric.jsx';
 import QuestionMultipleChoice from './QuestionMultipleChoice.jsx';
-import QuestionClonedCompareNumeric from './QuestionClonedCompareNumeric.jsx';
