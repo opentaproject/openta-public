3b6c24858bc153d54a348e635b5073c839ca5352
Hampus Linander <hampusl@gmail.com>
2017-02-28 15:38:42 +0100

    Added missing help component for multiple choice.

---
+++ frontend/app/components/questiontypes/HelpMultipleChoice.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component } from 'react';
+import uniqueId from 'lodash/uniqueId';
+
+export default class HelpMultipleChoice extends Component {
+  render = () => (
+  <span>
+<a data-uk-toggle={"{target:'#" + this.id + "'}"}><i className="uk-icon uk-icon-small uk-icon-question-circle-o uk-margin-small-left"/></a>
+<div id={this.id} className="uk-hidden">
+  <p>Välj ett eller flera alternativ och tryck sedan på <i className="uk-icon uk-icon-send uk-margin-small-left uk-margin-small-right"/> för att kontrollera. </p>
+</div>
+  </span>
+);
+
+  componentWillMount = () => {
+    this.id = uniqueId('help');
+  } 
+}
