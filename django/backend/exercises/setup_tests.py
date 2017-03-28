73ae65c38d0476a0d484747da4fa9a9a45e28d89
Hampus Linander <hampusl@gmail.com>
2017-03-28 13:58:02 +0200

    Started unit test for audits. Added new roles and groups in test setup.

--- django/backend/exercises/setup_tests.py
+++ django/backend/exercises/setup_tests.py
@@ -4,6 +4,7 @@ from course.models import Course
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 import exercises.paths as paths
+from PIL import Image
 
 
 def create_database(password="pw"):
@@ -19,19 +20,26 @@ def create_database(password="pw"):
     author, created = Group.objects.get_or_create(name="Author")
     author.permissions.add(perm_edit_exercise)
     author.save()
+    view, created = Group.objects.get_or_create(name="View")
+    view.save()
     u1 = User.objects.create_user('student1', 'student1@test.se', 'pw')
     u2 = User.objects.create_user('student2', 'student2@test.se', 'pw')
     uadmin = User.objects.create_superuser('admin1', 'admin1@test.se', 'pw')
+    usuper = User.objects.create_superuser('super', 'admin1@test.se', 'pw')
     student.user_set.add(u1)
     student.user_set.add(u2)
     admin.user_set.add(uadmin)
-    author.user_set.add(uadmin)
+    view.user_set.add(uadmin)
+    admin.user_set.add(usuper)
+    author.user_set.add(usuper)
+    view.user_set.add(usuper)
 
 
 def create_exercise(directory, name):
     path = os.path.join(directory.name, name)
     os.makedirs(path)
     exercise_path = os.path.join(path, "exercise.xml")
+    image_path = os.path.join(path, "figure.png")
     print(exercise_path)
     with open(exercise_path, "w") as f:
         f.write(
@@ -39,11 +47,15 @@ def create_exercise(directory, name):
                 <exercise>\n
                 <exercisename>Exercise1</exercisename>\n
                 <text>Test exercise text</text>\n
-                <question type="compareNumeric">\n
+                <figure>figure.png</figure>\n
+                <question type="compareNumeric" key="1">\n
                 <text>compareNumeric</text>\n
                 <expression>sin(2)</expression>\n
                 </question>\n
                 </exercise>\n
                 """
         )
+    image = Image.new('RGBA', size=(100, 100), color=(0, 255, 0))
+    with open(image_path, 'wb') as f:
+        image.save(f, 'PNG')
     return os.path.join(name)
---
+++ django/backend/exercises/test_audit.py
@@ -0,0 +1,142 @@
+from django.test import TestCase, LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.remote_connection import LOGGER
+from tempfile import TemporaryDirectory
+import time
+import random
+import os
+import logging
+import datetime
+
+LOGGER.setLevel(logging.WARNING)
+
+import exercises.paths as paths
+from exercises.setup_tests import create_exercise, create_database
+from .models import Exercise, ExerciseMeta, Question, Answer, ImageAnswer, AuditExercise
+from django.utils import timezone
+
+
+class AuditTest(StaticLiveServerTestCase):
+    def setUp(self):
+        create_database()
+        self.dir = TemporaryDirectory()
+        exercise_path = create_exercise(self.dir, 'exercise1')
+        paths.EXERCISES_PATH = self.dir.name
+        for msg in Exercise.objects.sync_with_disc():
+            print(msg)
+        # Exercise.objects.add_exercise('exercise1')
+        self.selenium = webdriver.Chrome()
+        self.selenium.implicitly_wait(0)
+        super().setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super().tearDown()
+
+    def change_exercise_options(self):
+        exercise = Exercise.objects.all()[0]
+        exercise.meta.published = True
+        exercise.meta.required = True
+        exercise.meta.image = True
+        exercise.meta.deadline_date = timezone.now() + datetime.timedelta(days=2)
+        exercise.meta.save()
+
+    def open_site(self):
+        sel = self.selenium
+        sel.get(self.live_server_url)
+
+    def login(self, username="student1", pw="pw", assert_role="student"):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        input_username = sel.find_element_by_css_selector('input[id=id_username]')
+        input_password = sel.find_element_by_css_selector('input[id=id_password]')
+        login = sel.find_element_by_css_selector('input[type=submit]')
+        input_username.send_keys(username)
+        input_password.send_keys(pw)
+        login.click()
+        wait.until(EC.text_to_be_present_in_element((By.ID, 'app'), assert_role))
+        assert assert_role in sel.page_source
+
+    def logout(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        sel.find_element_by_xpath('//a[contains(@href,\'logout\')]').click()
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, '//form[contains(@action, \'login\')]'))
+        )
+
+    def first_exercise(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        exercises = sel.find_elements_by_css_selector('li.course-exercise-item')
+        exercise = exercises[0]
+        exercise.click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1.uk-article-title')))
+
+    def answer(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        input_box = sel.find_elements_by_css_selector('textarea')[0]
+        input_box.send_keys('sin(2)')
+        # send_button = sel.find_elements_by_css_selector('a.uk-button')[0]
+        send_button = sel.find_elements_by_xpath('//a[.//i[contains(@class, \'uk-icon-send\')]]')[0]
+        send_button.click()
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'form'), "korrekt"))
+
+    def upload_image(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        input_image = sel.find_elements_by_xpath('//input[@type=\'file\']')[0]
+        input_image.send_keys(os.path.abspath('testdata/test_image.jpg'))
+        wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//img[contains(@src, \'imageanswerthumb\')]')
+            )
+        )
+
+    def back_to_course(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        back = sel.find_element_by_css_selector('ul.exercise-menu > li.uk-nav-header > a')
+        back.click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.course-exercise-item')))
+
+    def wait(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 500)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'wait_forever')))
+
+    def audit(self):
+        sel = self.selenium
+        wait = WebDriverWait(sel, 2)
+        sel.find_element_by_xpath('//a[contains(text(), \'Audit\')]').click()
+        wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//span[text()[contains(., \'1\')] and text()[contains(.,\'unaudited\')]]',
+                )
+            )
+        )
+
+    def test_1_student_answer_and_upload_image(self):
+        '''
+        Publish an exercise and verify that a student can answer and upload an image.
+        '''
+        self.open_site()
+        self.change_exercise_options()
+        self.login()
+        self.first_exercise()
+        self.answer()
+        self.upload_image()
+        self.logout()
+
+        self.open_site()
+        self.login('super', 'pw', 'admin')
+        self.first_exercise()
+        self.audit()
