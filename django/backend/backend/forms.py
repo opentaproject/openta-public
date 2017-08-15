08614cc213457193cdb448c0442491ad0e3e7e5b
Hampus Linander <hampusl@gmail.com>
2017-08-15 16:56:39 +0200

    Added first and last name to domain locked registration (and translations). Added problem button at login screen.

--- django/backend/backend/forms.py
+++ django/backend/backend/forms.py
@@ -36,10 +36,12 @@ class UserCreateFormDomain(ModelForm):
     """
 
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ("email",)
+        fields = ("email", "first_name", "last_name")
 
     def clean_email(self):
         domains = Course.objects.registration_domains()
