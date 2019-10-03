ae9f072461e76afee972846b9159ece1a6ec3f46
Stellan Ostlund <stellan.ostlund@gmail.com>
2019-10-03 15:03:32 +0200

    Fix headless runtests

--- django/backend/backend/selenium_utils/__init__.py
+++ django/backend/backend/selenium_utils/__init__.py
@@ -6,5 +6,5 @@ def create_selenium(*args, **kwargs):
     options = webdriver.ChromeOptions()
     if getattr(settings, 'HEADLESS', False):
         options.add_argument('headless')
-    options.add_argument('window-size=800x600')
+    options.add_argument('window-size=1280x1024')
     return webdriver.Chrome(chrome_options=options)
