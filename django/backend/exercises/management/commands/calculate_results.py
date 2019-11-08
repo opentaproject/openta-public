84652beea3db57372727257927e898586559f03a
Stellan Ostlund <stellan.ostlund@gmail.com>
2019-11-08 14:11:50 +0100

    Restrict calculate_results to published courses

--- django/backend/exercises/management/commands/calculate_results.py
+++ django/backend/exercises/management/commands/calculate_results.py
@@ -12,9 +12,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info('Started calculating results and statistics')
-        for course in Course.objects.filter(published=True):
-            logger.info("Calculating for course {}".format(course.course_name))
-            student_statistics_exercises(force=True, course=course)
-            logger.info('Statistics done, now doing results.')
-            students_results(force=True, course=course)
-            logger.info('Finished calculating results and statistics')
+        for course in Course.objects.all():
+            if course.published:
+                logger.info("Calculating for course {}".format(course.course_name))
+                student_statistics_exercises(force=True, course=course)
+                logger.info('Statistics done, now doing results.')
+                students_results(force=True, course=course)
+                logger.info('Finished calculating results and statistics')
+            else:
+                logger.info("Skip calculating results for unpublished course {}".format( course.course_name ) )
