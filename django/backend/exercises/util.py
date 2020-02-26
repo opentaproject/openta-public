65cce8dfae0950b0e641dff1d45ae728543fa7bf
Stellan Ostlund <stellan.ostlund@gmail.com>
2020-02-26 12:12:53 +0100

    update util.py

--- django/backend/exercises/util.py
+++ django/backend/exercises/util.py
@@ -29,3 +29,28 @@ def get_hash_from_string(string):
             c = chr(int(c) + 97 ) 
         res = res + c
     return res 
+
+def index_of_matching_right_paren(beg, expression):
+    level = 1
+    ind = beg + 1
+    while level > 0 and ind < len(expression):
+        if expression[ind] == ')':
+            level = level - 1
+        elif expression[ind] == '(':
+            level = level + 1
+        ind = ind + 1
+    assert expression[beg] == '(', 'LEFT PAREN WRONG'
+    assert expression[ind-1] == ')', 'RIGHT PAREN WRONG'
+    return ind 
+
+def index_of_matching_right_paren(beg, expression):
+    level = 1
+    ind = beg + 1
+    while level > 0 and ind < len(expression):
+        if expression[ind] == ')':
+            level = level - 1
+        elif expression[ind] == '(':
+            level = level + 1
+        ind = ind + 1
+    return ind - 1
+
