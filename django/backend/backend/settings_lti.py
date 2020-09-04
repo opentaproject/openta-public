1f4b8428d73d581b707a3383f3adea1d053f0407
Stellan Ostlund <stellan.ostlund@gmail.com>
2020-09-04 11:23:25 +0200

    Fix default FORCE_ROLE_TO_STUDENT to True

--- django/backend/backend/settings_lti.py
+++ django/backend/backend/settings_lti.py
@@ -7,4 +7,8 @@ TEMPLATES[0]['DIRS'].append(os.path.join(BASE_DIR, 'opentalti', 'templates'))
 AUTHENTICATION_BACKENDS = ['opentalti.apps.LTIAuth', 'django.contrib.auth.backends.ModelBackend']
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 TIME_ZONE = 'Europe/Copenhagen'
-FORCE_ROLE_TO_STUDENT = False
+
+# THE NEXT FLAG FORCES ALL NON-STUDENT Roles to Student
+# THIS IS IMPORTANT SAFETY FEATURE SO THAT LOCAL MISCONFIGURATIONS
+# DO NOT ACCIDENTALL COMPROMISE OpenTA
+FORCE_ROLE_TO_STUDENT = True 
