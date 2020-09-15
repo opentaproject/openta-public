cca19b47c60997d61eb70b25635e31ca511ebaca
Stellan Ostlund <stellan.ostlund@gmail.com>
2020-09-15 12:31:33 +0200

    Do not allow duplicate identities

--- django/backend/opentalti/apps.py
+++ django/backend/opentalti/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
+from django.db import transaction, IntegrityError
 from users.models import OpenTAUser
 from django.contrib.auth.models import User, Group
 from django.conf import settings
+import datetime
 from course.models import Course
 from exercises.modelhelpers import enrollment
 from pylti.common import LTIException
@@ -15,6 +17,7 @@ from .admin import (
 )
 import re
 import time
+import json
 from django.contrib import messages
 import logging
 
@@ -27,9 +30,12 @@ class OpentaltiConfig(AppConfig):
 
 def create_new_user(request, username, course):
     logging.debug("CREATE NEW USER username" + str(username) + str(course))
-    assert course.pk >= 0
-    assert not (username == None)
-    assert not (User.objects.filter(username=username).exists())
+    if course.pk == 0 :
+        raise LTIException("COURSE PK = 0 ")
+    if username == None :
+        raise LTIException("USERNAME = None ")
+    if User.objects.filter(username=username).exists() :
+        raise LTIException("Username exists and cannot be recreated" )
     user = User.objects.create(username=username)
     roles = request.POST.get("roles", "Learner")
     opentauser = OpenTAUser.objects.create(user=user)
@@ -102,16 +108,27 @@ def update_user_profile(user, user_stub):
 
 
 def get_or_create_user(request, course):
+    fp = open("/tmp/requests.txt",'a')
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    fp.write( now + ':' + str( request.body.decode('utf-8') ) )
+    fp.write("\n")
+    fp.close()
     user_stub = user_stub_from_request(request, course)
     user = None
+    user_exists = False
     try:
         opentauser = OpenTAUser.objects.get(immutable_user_id=user_stub.immutable_user_id)
         opentauser.courses.add(course)
         opentauser.save()
         user = opentauser.user
+        user_exists = True
     except OpenTAUser.DoesNotExist:
-        user = create_new_user(request, user_stub.immutable_user_id, course)
-
+        user_exists = False
+    try: 
+        if not user_exists :
+            user = create_new_user(request, user_stub.immutable_user_id, course)
+    except Exception as e :
+        raise LTIException('Error 122: Error creating user %s  from the hash %s .  The most likely reason is that you have two Canvas identities and have accessed OpenTA previously through another.  Alternatively your Canvas identity has changed but the username is unchanged. Either make sure you use the proper identity or else ask the course admin to change or delete the previous username. ' % ( str(user_stub.lis_person_name_full )  , '{:<30}'.format( str( user_stub.immutable_user_id) ) ) )
     if not user.is_staff:
         update_user_profile(user, user_stub)
     return user
@@ -132,14 +149,17 @@ def course_from_request(request):
 
 
 class LTIAuth:
+
+    @transaction.atomic 
     def ltiauth(self, request):
         logging.debug("LTIAUTH LTIAUTH CALLED")
         verify_request(request)
         course = course_from_request(request)  # TODO REMOVE DEPENDENCE ON REQUEST PARSE
         try:
-            user = get_or_create_user(request, course)
-        except:
-            raise LTIException("Authentication failed")
+            with transaction.atomic():
+                user = get_or_create_user(request, course)
+        except Exception as e :
+            raise LTIException( "%s " % ( "Authentication failed {0}".format(e)  ))
         return user
 
     def authenticate(self, request, username=None, password=None):
