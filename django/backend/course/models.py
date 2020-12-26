0328d34c98775ff7361aba7e03c087f43e03b38d
Stellan Ostlund <stellan.ostlund@gmail.com>
2020-12-26 23:37:48 +0100

    Dump the course.json when a course is saved

--- django/backend/course/models.py
+++ django/backend/course/models.py
@@ -1,4 +1,5 @@
 import os
+from django.core.management import call_command
 import logging
 import datetime
 import uuid
@@ -166,9 +167,10 @@ class Course(models.Model):
             defaultuser.opentauser.courses.add(self)
         except:
             pass
-        
-
-        
+        dump_path = paths.EXERCISES_PATH + '/' + str(self.course_key) + ".json" 
+        output = open(dump_path,'w') # Point stdout at a file for dumping data to.
+        call_command('dumpdata','course',format='json',indent=3,stdout=output)
+        output.close()        
 
     def clean(self):
         if False and self.use_auto_translation  and self.google_auth_string == '':
