d5f734a1871edcbe3d0f835ca401de94f001056a
Stellan Ostlund <stellan.ostlund@gmail.com>
2021-01-28 17:17:01 +0100

    Router works

--- django/backend/backend/routers/__init__.py
+++ django/backend/backend/routers/__init__.py
@@ -1,32 +1,69 @@
 from django.conf import settings
+from aggregation.models import Aggregation
+from course.models import Course
+from django.contrib.auth.models import Group, User
+from exercises.models import Answer, AuditExercise, ImageAnswer, Question, Answer, ExerciseMeta, Exercise
+from django.contrib.sessions.models import Session
+from users.models import OpenTAUser
 
+
+dbmodels = [ Aggregation, Course, Group, Answer, AuditExercise, Exercise, ExerciseMeta,  \
+    ImageAnswer, Question, OpenTAUser, Aggregation,  ExerciseMeta]
+
+# django.core.cache.backends.db.BaseDatabaseCache.__init__.<locals>.CacheEntry,\
 class AuthRouter:
     """
     A router to control all database operations on models in the
     auth and contenttypes applications.
     """
-    route_app_labels = {'auth', 'contenttypes','users','course',}
+    route_app_labels = {'auth', 'contenttypes','users','course','backend','opentalti'}
 
 
-    def db_for_read(self, model, **hints):
+    def db_for_read(self, model=None, **hints):
         """
         Attempts to read auth and contenttypes models go to default.
         """
-        #print("HINTS READ = ", hints )
-        return settings.DB_NAME
+        #print("SELF = ", self)
+        #if not model  == None:
+        #    print("model read = ", model)
+        #else :
+        #    print("model read = None")
+        #if model == None :
+        #    return settings.DB_NAME
+        if model in dbmodels:
+            try: 
+                print("READ MODEL FOUND", model, "HINTS" , hints)
+            except:
+                print("READ MODEL FOUND", "NO HINTS\n")
+            return settings.DB_NAME
+        else:
+            return 'default'
 
-    def db_for_write(self, model, **hints):
+    def db_for_write(self, model=None, **hints):
         """
         Attempts to write auth and contenttypes models go to default.
         """
-        #print("HINTS WRITE = ", hints )
-        return settings.DB_NAME
+        #print("SELF = ", self )
+        #if not model  == None:
+        #    print("model write = ", model)
+        #else :
+        #    print("model write = None")
+        if model in dbmodels:
+            
+            try: 
+                print("WRITE MODEL FOUND", model, "HINTS" , hints )
+            except:
+                print("WRITE READ MODEL FOUND", model )
+            return settings.DB_NAME
+        else:
+            return 'default'
 
     def allow_relation(self, obj1, obj2, **hints):
         """
         Allow relations if a model in the auth or contenttypes apps is
         involved.
         """
+        print("ALLOW RELATION")
         return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
@@ -34,4 +71,10 @@ class AuthRouter:
         Make sure the auth and contenttypes apps only appear in the
         settings.DB_NAME database.
         """
+        print("APP_LABEL = ", app_label)
+        print("ALLOW MIGRATE")
+        print("MIGRATE model ", model_name)
+        
+        #if  hints == {} :
+        #   return 'default'
         return settings.DB_NAME
