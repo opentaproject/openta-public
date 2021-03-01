8eb3302db61c33def92cfc2a030f02da8b61695b
Stellan Ostlund <stellan.ostlund@gmail.com>
2021-03-01 01:34:37 +0100

    Make subdomain relocacatble with restore

---
+++ django/backend/course/management/commands/repair_subdomain.py
@@ -0,0 +1,17 @@
+"""Remove name and surname from users."""
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import logging
+from opentasites.models import OpenTASite
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        """Remove name and surname from users."""
+        print("SETTINGS SUBDOMAIN = %s " % settings.SUBDOMAIN)
+        for opentasite in OpenTASite.objects.all():
+            print("SUBDOMAIN = %s %s %s " % ( opentasite.subdomain, opentasite.db_name, opentasite.db_label ) )
+            opentasite.subdomain = settings.SUBDOMAIN
+            opentasite.db_label = 'moved from ' + opentasite.db_label 
+            opentasite.save()
